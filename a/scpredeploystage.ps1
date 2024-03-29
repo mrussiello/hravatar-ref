d8509baf8862e7973e3a36825629d7d6cff563a2
miker <miker@192.168.1.93>
2024-03-29 11:02:13 -0400

    2024CR332	3/29/2024	MikeR	Feature	Stage - need to be able to redeploy apps to Stage via powershell

---
+++ a/scpredeploystage.ps1
@@ -0,0 +1,41 @@
+
+ant copystaticfilesfordeploy
+
+$app="tm2ref2"
+$contextroot="tr"
+$baseurl="stage1.hravatar.com"
+
+
+$prefix="${app}-1.0"
+$identityfile="C:/dev/AmazonAWS/HRA-Stage-Oregon.pem"
+$glassfishlocation="/usr/pgms/payara6.2023.12/payara6/glassfish"
+$basedir="c:/work/${app}"
+$targetdir="$basedir/target"
+
+$DateTime = (Get-Date -Format "MM-dd-yyyy")
+$proddistdir="/backup/dist/$DateTime"
+
+# make dist dir
+ssh -o "StrictHostKeyChecking=accept-new" -i "${identityfile}" ec2-user@${baseurl} "mkdir $proddistdir"
+
+
+# Copy static files
+scp -i "${identityfile}" -r "${basedir}/deploytemp/webmod"  ec2-user@${baseurl}:/work/${app}
+
+
+# copy war file
+
+# copy file to dist dir
+scp -i "${identityfile}"  "${targetdir}/${prefix}.war"  ec2-user@${baseurl}:/backup/dist/$DateTime
+
+# undeploy
+
+
+ssh -i "${identityfile}" ec2-user@${baseurl} "$glassfishlocation/bin/asadmin  --user admin --passwordfile /home/payara/passwd.gf undeploy ${prefix}"
+
+# deploy
+ssh -i "${identityfile}" ec2-user@${baseurl} "$glassfishlocation/bin/asadmin --user admin --passwordfile /home/payara/passwd.gf deploy --virtualservers server --contextroot ${contextroot} --force=true ${proddistdir}/${prefix}.war"
+
+
+
+
